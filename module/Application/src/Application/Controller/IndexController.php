3b8070874a397985b5b7009e42f2600a32f556d3
Efim <Daemefim@gmail.com>
2015-01-27 20:38:15 +0300

    no message

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -11,11 +11,17 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
         return new ViewModel();
+    }
+
+    public function ajaxAction()
+    {
+
     }
 }
